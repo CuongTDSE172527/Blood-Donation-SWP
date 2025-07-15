ee4f976aa3159c331660642815ba9ea9c495d7dc
nhutruong47 <truongthainhuu@gmail.com>
2025-07-15 17:15:31 +0700

    gan noi bloodInventory vs user

---
+++ src/main/java/com/example/demo1/entity/enums/Gender.java
@@ -0,0 +1,8 @@
+// package: com.example.demo1.entity.enums
+package com.example.demo1.entity.enums;
+
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
--- src/main/java/com/example/demo1/repo/BloodInventoryRepository.java
+++ src/main/java/com/example/demo1/repo/BloodInventoryRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo1.repo;
 
 import com.example.demo1.entity.BloodInventory;
+import com.example.demo1.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BloodInventoryRepository extends JpaRepository<BloodInventory, Long> {
     Optional<BloodInventory> findByBloodType(String bloodType);
+    List<BloodInventory> findByUpdatedBy(User user);
 }
-
